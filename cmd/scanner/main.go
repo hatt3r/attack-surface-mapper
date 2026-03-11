ae4350f8f191fbba1d8cba5696600a88960bfa2a
Serene Mathew <serene719@gmail.com>
2026-03-11 20:26:26 +0000

    comments for main and scanner

--- cmd/scanner/main.go
+++ cmd/scanner/main.go
@@ -4,24 +4,29 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/hatt3r/attack-surface-mapper/internal/fingerprint"
+	"github.com/hatt3r/attack-surface-mapper/internal/risk"
 	"github.com/hatt3r/attack-surface-mapper/internal/scanner"
 )
 
 func main() {
 
-	if len(os.Args) < 2 {
+	if len(os.Args) < 2 || len(os.Args) > 2 {
 		fmt.Println("Usage: scanner <target-ip>")
 		return
 	}
 
 	target := os.Args[1]
 
-	fmt.Println("Scanning target:", target)
+	fmt.Println("Scanning target:")
+	fmt.Println("Target:", target)
 
 	openPorts := scanner.ScanTarget(target)
 
-	fmt.Println("Open Ports:")
+	fmt.Println("Open Portsdetected \n:")
 	for _, port := range openPorts {
-		fmt.Println(port)
+		service := fingerprint.IdentifyService(port)
+		risklevel := risk.Score(service)
+		fmt.Printf("Port: %d -> %s, Risk Level: %s\n", port, service, risklevel)
 	}
 }
--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -11,13 +11,17 @@ func ScanTarget(target string) []int {
 
 	var openPorts []int
 	var wg sync.WaitGroup
+	// allows to wait for all goroutines to finish
 	var mutex sync.Mutex
+	// prevents race conditions for multiple goroutines accessing
 
 	ports := make(chan int, 100)
 
 	for i := 0; i < cap(ports); i++ {
 		wg.Add(1)
+
 		go worker(target, ports, &openPorts, &mutex, &wg)
+		// worker goroutines starts for each channel
 	}
 
 	for port := 1; port <= 1024; port++ {
@@ -31,8 +35,11 @@ func ScanTarget(target string) []int {
 	return openPorts
 }
 
+// goroutine worker that scans ports from channel
 func worker(target string, ports chan int, openPorts *[]int, mutex *sync.Mutex, wg *sync.WaitGroup) {
+
 	defer wg.Done()
+	// signals that the goroutine has finished its work when the function returns
 
 	for port := range ports {
 
@@ -44,6 +51,7 @@ func worker(target string, ports chan int, openPorts *[]int, mutex *sync.Mutex, 
 			conn.Close()
 
 			mutex.Lock()
+			//helps to prevent race conditions when multiple goroutines access at the same time
 			*openPorts = append(*openPorts, port)
 			mutex.Unlock()
 		}
